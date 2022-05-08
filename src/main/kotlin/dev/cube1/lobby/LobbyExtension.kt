e052870f17897e39b999e04d7adb7dc807d1dc8a
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2022-05-09 01:56:16 +0900

    Add main feature to extension main

--- src/main/kotlin/dev/cube1/lobby/LobbyExtension.kt
+++ src/main/kotlin/dev/cube1/lobby/LobbyExtension.kt
@@ -1,10 +1,14 @@
 package dev.cube1.lobby
 
+import dev.cube1.lobby.listener.Listener
+import dev.cube1.lobby.task.ParticleTask
 import net.minestom.server.extensions.Extension;
 
 class LobbyExtension : Extension() {
 
     override fun initialize(): LoadStatus {
+        ParticleTask.run()
+        Listener.run(eventNode())
         logger().info("[LobbyExtension] has been enabled!")
 
         return LoadStatus.SUCCESS
