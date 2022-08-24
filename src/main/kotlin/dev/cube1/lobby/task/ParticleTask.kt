1102fecde68ddfe53426f049b20cae106cd02277
Project_TL <me@projecttl.net>
2022-08-24 15:19:45 +0900

    fix: race particle location

--- src/main/kotlin/dev/cube1/lobby/task/ParticleTask.kt
+++ src/main/kotlin/dev/cube1/lobby/task/ParticleTask.kt
@@ -14,7 +14,7 @@ import world.cepi.particle.showParticle
 object ParticleTask {
 
     val particles = listOf(
-        Pair(Vec(17.5, 112.5, -13.5), Vec(15.5, 112.5, -11.5)),
+        Pair(Vec(39.5, 113.5, 5.5), Vec(41.5, 113.5, 7.5)),
         Pair(Vec(16.5, 113.5, 15.5), Vec(14.5, 113.5, 17.5)),
         Pair(Vec(13.5, 112.5, -0.5), Vec(15.5, 112.5, -2.5)))
 
