cc0984bff4161541a778ed10876d53a7187afd47
Project_TL <me@projecttl.net>
2022-08-25 15:50:15 +0900

    feat: npc type changed to player

--- src/main/kotlin/dev/cube1/lobby/task/NPCTask.kt
+++ src/main/kotlin/dev/cube1/lobby/task/NPCTask.kt
@@ -8,6 +8,7 @@ import net.kyori.adventure.text.format.TextDecoration
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.Entity
 import net.minestom.server.entity.EntityType
+import net.minestom.server.entity.fakeplayer.FakePlayer
 import java.util.*
 
 data class ServerNPC(val name: Component, val server: String, val loc: Pos, val uniqueId: UUID = UUID.randomUUID())
@@ -18,19 +19,27 @@ object NPCTask {
             "<bold><green>야생".toMini(),
             "wild",
             Pos(43.5, 113.0, 17.5, -180F, 0F),
+            UUID.fromString("d5c1b63b-4554-48cc-9322-44cae6193b0a")
         )
     )
 
     fun run() {
         entityList.forEach { npc ->
-            val entity = Entity(EntityType.VILLAGER).let { e ->
-                val meta = e.entityMeta
+//            val entity = Entity(EntityType.VILLAGER).let { e ->
+//                val meta = e.entityMeta
+//                meta.customName = npc.name
+//                meta.isCustomNameVisible = true
+//
+//                e
+//            }
+//            entity.setInstance(instance, npc.loc)
+            FakePlayer.initPlayer(UUID.randomUUID(), npc.server) { fakePlayer ->
+                val meta = fakePlayer.entityMeta
                 meta.customName = npc.name
                 meta.isCustomNameVisible = true
 
-                e
+                fakePlayer.setInstance(instance, npc.loc)
             }
-            entity.setInstance(instance, npc.loc)
         }
     }
 }
